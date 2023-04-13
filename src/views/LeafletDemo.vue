9e3ccc64633f2689f38fb399adeae9273d6ab134
yangyu2130 <1416916853@qq.com>
2023-04-13 23:19:13 +0800

    [测距和绘制图形中的矩形已完成]

--- src/views/LeafletDemo.vue
+++ src/views/LeafletDemo.vue
@@ -23,32 +23,55 @@
                 <i v-if="!optLayerVif" class="el-icon-arrow-down"></i>
                 <i v-if="optLayerVif" class="el-icon-arrow-up"></i>
             </div>
-            <div class="topOptDiv" @click="optshowClick">显示
-                <i class="el-icon-arrow-down"></i>
+            <div class="topOptDiv optShowDiv" @click="optshowClick">显示
+                <!-- 显示 包含： 标记 收藏 点亮-->
+                <div v-if="optShowVif" class="optShow">
+                    <!-- 标记 -->
+                    <div class="showDiv" @click="showMarkClick">
+                        <i class="el-icon-paperclip"></i>
+                        标记
+                    </div>
+                    <div class="showDiv" @click="showCollectClick">
+                        <i class="el-icon-location-outline"></i>
+                        收藏
+                    </div>
+                    <div class="showDiv" @click="showLightClick">
+                        <i class="el-icon-star-off"></i>
+                        点亮
+                    </div>
+                </div>
+                <i v-if="!optShowVif" class="el-icon-arrow-down"></i>
+                <i v-if="optShowVif" class="el-icon-arrow-up"></i>
             </div>
             <div class="topOptDiv optToolDiv" @click="optToolClick">工具箱
                 <div v-if="optToolVif" class="optTool">
                     <!-- 工具箱 包含: 测距 标记 收藏 点亮 绘制图形 -->
                     <!-- 测距 -->
-                    <div class="distanceDiv" @click="distanceClick">
+                    <div class="toolDiv" @click="distanceClick">
                         <i class="el-icon-paperclip"></i>
                         测距
                     </div>
-                    <div class="markDiv" @click="markClick">
+                    <div class="toolDiv" @click="markClick">
                         <i class="el-icon-location-outline"></i>
                         标记
                     </div>
-                    <div class="collectDiv" @click="collectClick">
+                    <div class="toolDiv" @click="collectClick">
                         <i class="el-icon-star-off"></i>
                         收藏
                     </div>
-                    <div class="lightDiv" @click="lightClick">
+                    <div class="toolDiv" @click="lightClick">
                         <i class="el-icon-s-opportunity"></i>
                         点亮
                     </div>
-                    <div class="drawDiv" @click="drawClick">
+                    <!-- 绘制图形分为： 矩形、圆形、自定义 -->
+                    <div class="toolDiv" @mouseover="drawToolHover = true">
                         <i class="el-icon-news"></i>
                         绘制图形
+                    </div>
+                    <div class="drawGraphDiv" v-if="drawToolHover"  @mouseleave="drawToolHover = false">
+                        <span @click="drawGraphClick('rectangle')">矩形</span>
+                        <span @click="drawGraphClick('circular')">圆形</span>
+                        <span @click="drawGraphClick('costom')">自定义</span>
                     </div>
                 </div>
                 <i v-if="!optToolVif" class="el-icon-arrow-down"></i>
@@ -65,6 +88,7 @@
 <script>
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import { all } from 'q'
 export default {
     name: 'LeafletDemo',
     comments: {
@@ -85,7 +109,9 @@ export default {
                 DT: null,
                 YX: null,
                 LW: null
-            }
+            },
+            optShowVif: false,
+            drawToolHover: false
         }
     },
     mounted() {
@@ -187,8 +213,11 @@ export default {
                 this.optLayerVif = !this.optLayerVif
             }
         },
-        optshowClick() {
-            
+        // 点击 显示
+        optshowClick(e) {
+            if(e.target.className === 'el-icon-arrow-up' || e.target.className.includes('optShowDiv')) {
+                this.optShowVif = !this.optShowVif
+            }
         },
         // 点击工具箱
         optToolClick(e) {
@@ -199,25 +228,92 @@ export default {
         // 工具箱 点击测绘
         distanceClick() {
             // 点击测绘, 开始测绘事件
-            this.distanceFun(0)
+            this.distanceFun()
         },
-        distanceFun(time) {
+        // 测绘函数
+        distanceFun() {
             // 用time来记录点击点数
             let that = this
+            var time = 0
             var myIcon = L.divIcon({className: 'myMarkerIcon'});
-            this.map.on('click', function(e){
-                const data = [e.latlng.lat*1,e.latlng.lng*1]
+            var transparentIcon = L.divIcon({className: 'transparentIcon'});
+            const allpoint = []
+            var transparentPolyLine = null
+            var transparentPolyLineToolTip = null
+            that.map.on('click', function(e){
+                const nowClickPoint = [e.latlng.lat*1,e.latlng.lng*1]
+                allpoint.push(nowClickPoint)
                 if(time == 0) {
-                    const aa =  L.marker(data,{
+                    // 起点
+                    const aa =  L.marker(nowClickPoint,{
                         icon: myIcon,
                     }).addTo(that.map)
+                    // bindTooltip 显示在右侧 比较小
                     aa.bindTooltip(`<span class="markerTooltipSpan">起点</span>`,{
                         permanent: true,
                         offset: [10,0]
                     }).openTooltip();
+                    // bindPopup 显示在上测 比较大
                     // aa.bindPopup(`<span>aaa</span>`).openPopup();
+                    that.map.on('mousemove', function(e) {
+                        // 路径透明线
+                        let point = [e.latlng.lat*1,e.latlng.lng*1]
+                        let polylinePoint = [[e.latlng.lat*1,e.latlng.lng*1],allpoint[allpoint.length-1]]
+                        if(transparentPolyLine !== null){
+                            transparentPolyLine.remove()
+                        }
+                        transparentPolyLine = L.polyline(polylinePoint, {color: 'rgba(255,0,0,0.3)'}).addTo(that.map);
+                        // 计算距离（这里的距离应为： 全部折线的距离总和 + mousemove移动的距离）
+                        let nowDistance = 0
+                        allpoint.map((item, index) => {
+                            if(index !== allpoint.length-1) {
+                                nowDistance += that.map.distance(allpoint[index*1],allpoint[index*1+1]).toFixed(1)*1
+                            }
+                        })
+                        nowDistance += that.map.distance(allpoint[allpoint.length - 1],point).toFixed(1)*1
+                        if(transparentPolyLineToolTip !== null) {
+                            transparentPolyLineToolTip.remove()
+                        }
+                        transparentPolyLineToolTip =  L.marker(point,{icon: myIcon,}).addTo(that.map)
+                        transparentPolyLineToolTip.bindTooltip(`<span class="markerTooltipSpan">总长： ${nowDistance} 米; 双击结束</span>`,{
+                            permanent: true,
+                            offset: [10,0]
+                        }).openTooltip();
+                    })
+                    // 双击 暂停测距
+                    that.map.on('dblclick', function(e) {
+                        that.stopDistanceFun()
+                        transparentPolyLineToolTip.remove()
+                    })
+                } else {
+                    // 中间点
+                    // 画 红色折线
+                    let polyLinePoint = [allpoint[allpoint.length-2],allpoint[allpoint.length-1]]
+                    L.polyline(polyLinePoint, {color: 'rgba(255,0,0,0.5)'}).addTo(that.map)
+                    // 计算距离（这里的距离应为： 全部折线的距离总和）
+                    let nowDistance = 0
+                    allpoint.map((item, index) => {
+                        if(index !== allpoint.length-1) {
+                            nowDistance += that.map.distance(allpoint[index*1],allpoint[index*1+1]).toFixed(1)*1
+                        }
+                    })
+                    // 显示 距离
+                    let aa =  L.marker(nowClickPoint,{
+                        icon: myIcon,
+                    }).addTo(that.map)
+                    aa.bindTooltip(`<span class="markerTooltipSpan">总长： ${nowDistance} 米</span>`,{
+                        permanent: true,
+                        offset: [10,0]
+                    }).openTooltip();
                 }
-            });
+                time++
+            })
+        },
+        // 暂停 测绘
+        stopDistanceFun() {
+            this.map.off('click')
+            this.map.off('mousemove')
+            this.map.off('dblclick')
         },
         // 工具箱 点击标记
         markClick() {
@@ -232,8 +328,59 @@ export default {
 
         },
         // 工具箱 绘制图形
-        drawClick() {
+        drawGraphClick(type) {
+            let that = this
+            if(type === 'rectangle'){
+                // 矩形
+                // 点击，确定其中一个顶点1，可向四周移动鼠标确定 对角顶点2
+                var myIcon = L.divIcon({className: 'myMarkerIcon'});
+                let rectanglePolyLine = null
+                let rectanglePolyLineToolTip = null
+                this.map.on('click', function(e) {
+                    // 起点
+                    let point1 = [e.latlng.lat*1,e.latlng.lng*1]
+                    // const aa =  L.marker(point1,{
+                    //     icon: myIcon,
+                    // }).addTo(that.map)
+                    that.map.on('mousemove', function(e) {
+                        let point2 = [e.latlng.lat*1,e.latlng.lng*1]
+                        if(rectanglePolyLine !== null) {
+                            rectanglePolyLine.remove()
+                        }
+                        let polyLinePoint1 = [point1,[point1[0],point2[1]],point2,[point2[0],point1[1]],point1]
+                        rectanglePolyLine = L.polyline(polyLinePoint1, {color: 'rgba(255,0,0,0.5)'}).addTo(that.map)
 
+                        if(rectanglePolyLineToolTip !== null) {
+                            rectanglePolyLineToolTip.remove()
+                        }
+                        rectanglePolyLineToolTip =  L.marker(point2,{icon: myIcon,}).addTo(that.map)
+                        rectanglePolyLineToolTip.bindTooltip(`<span class="markerTooltipSpan">单击重新选择顶点，双击结束</span>`,{
+                            permanent: true,
+                            offset: [10,0]
+                        }).openTooltip();
+                    })
+                    that.map.on('dblclick', function(e) {
+                        rectanglePolyLineToolTip.remove()
+                        that.stopDistanceFun()
+                    })
+                })
+            } else if(type === 'circular') {
+                // 圆形
+            } else if(type === 'costom') {
+                // 自定义
+            }
+        },
+        // 显示 标记
+        showMarkClick() {
+
+        },
+        // 显示 收藏
+        showCollectClick() {
+            
+        },
+        // 显示 点亮
+        showLightClick() {
+            
         },
     },
 }
@@ -341,6 +488,49 @@ export default {
                     text-align: center;
                 }
                 >div:hover{
+                    background:rgba(0,0,0,0.05);
+                    color: #409EFF;
+                    text-align: center;
+                }
+                .drawGraphDiv{
+                    width: 150px;
+                    position: absolute;
+                    right: 105px;
+                    bottom: 0px;
+                    background: #fff;
+                    span{
+                        background: #fff;
+                        padding: 0px 5px;
+                        color: #000;
+                    }
+                    >span:hover{
+                        color: #409EFF;
+                    }
+                }
+                .drawGraphDiv:hover{
+                    background: #fff;
+                    color: #000;
+                }
+            }
+            // 显示 下拉显示
+            .optShow{
+                border: 1px solid #E4E7ED;
+                background: #fff;
+                position: absolute;
+                top: 45px;
+                right: 100px;
+                z-index: 401;
+                height: 120px;
+                width: 90px;
+                text-align: left;
+                >div{
+                    color: #000;
+                    width: 90px;
+                    height: 40px;
+                    line-height: 40px;
+                    text-align: center;
+                }
+                >div:hover{
                     background:rgba(0,0,0,0.05) !important;
                     color: #409EFF;
                     text-align: center;
@@ -353,6 +543,14 @@ export default {
         height: 15px;
         border-radius: 50%;
         background: rgba(255,0,0,0.7);
+        z-index: 402;
+    }
+    /deep/ .transparentIcon{
+        width: 5px;
+        height: 5px;
+        border-radius: 50%;
+        background: rgba(255,0,0);
+        opacity: 0.1;
         z-index: 402;
     }
     /deep/ .markerTooltipSpan{
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 
 module.exports = {
-    chainWebpack: config => {
-      config.resolve.alias
-        .set("@", resolve("src"))
-        .set("@components", resolve("src/components"))
-        .set("@public", resolve("public"))
-        .set("@images", resolve("public/images"));
-    },
-    publicPath: '/',
+    // chainWebpack: config => {
+    //   config.resolve.alias
+    //     .set("@", resolve("src"))
+    //     .set("@components", resolve("src/components"))
+    //     .set("@public", resolve("public"))
+    //     .set("@images", resolve("public/images"));
+    // },
+    // publicPath: '/',
     devServer: {
         port: 9000,
         host: '0.0.0.0',
